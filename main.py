52ddde8dcf782aa39c4cda22ace82107068c8129
Eugenio Bravo <ebravo930@gmail.com>
2025-04-28 21:11:53 -0400

    se agrega get items

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ collection = database.items  # Cambiar si quieren otro nombre de colección
 async def read_root():
     return {"message": "Bienvenido a la API de MongoDB con FastAPI"}
 
+@app.get("/items/")
+async def get_items():
+    items = []
+    async for item in collection.find():
+        items.append(item)
+    return items
+
+
 @app.post("/items/")
 async def create_item(item: dict):
     new_item = await collection.insert_one(item)
